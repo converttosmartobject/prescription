98d8e5ac828b8713cb5f2157fb26887e061606ab
Asra Siddiqui <asrasiddiqui127@gmail.com>
2019-11-04 23:50:53 -0800

    b

--- sketch.js
+++ sketch.js
@@ -1,30 +1,38 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  text("Time's up. Your therapet has to see her next patient now. Good bye.");
-  prescription();
+  timesUp();
+}
+
+function timesUp() {
+  textStyle(NORMAL);
+  textSize(19);
+  text('Times Up. Your therapet has to see her next patient now. Goodbye.', 10, 30);
+   button = createButton('dont forget your prescription!');
+  button.position(60, 60);
+  button.mousePressed(prescription);
 }
 
 function prescription() {
   fill(255);
-  rect(50, 50, 300, 300, 20);
+  rect(80, 120, 300, 300, 20);
   textSize(28);
   textStyle(BOLD);
   fill(0)
-text('PRESCRIPTION', 120, 100);
+text('PRESCRIPTION', 150, 170);
     img = createImg('https://dejpknyizje2n.cloudfront.net/svgcustom/clipart/preview/29e81ea754c047c9a8d92c7040d219ea.png');
   img.hide();
-  image(img, 65,70,50, 50);
+  image(img, 95,140,50, 50);
   img = createImg('pawprintsign.png');
 img.hide();
-image(img, 220, 230, 80, 80);
+image(img, 250, 300, 80, 80);
   textStyle(ITALIC);
   fill(0, 102, 153);
-  text('get a real cat', 120, 190);
+  text('get a real cat', 150, 260);
   fill(0);
   textStyle(BOLD);
   textSize(10);
-  text('DOCTOR SIGNATURE', 210, 320);
+  text('DOCTOR SIGNATURE', 240, 390);
 }
